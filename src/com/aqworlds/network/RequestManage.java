2c8b9bf29b8345ad51b78176e1b4b2f16529ad08
Thalys Márcio <thalysmarciobn@icloud.com>
2017-05-05 16:17:11 -0300

    Change 1#

--- src/com/aqworlds/network/RequestManage.java
+++ src/com/aqworlds/network/RequestManage.java
@@ -1,5 +1,7 @@
 package com.aqworlds.network;
 
+import com.aqworlds.network.events.FirstJoin;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,6 +17,16 @@ public class RequestManage {
     }
 
     public void registry() {
+        this.request(new FirstJoin());
+    }
 
+    private void request(MessageHandler handler) {
+        if (handler.getClass().getAnnotations().length > 0) {
+            Event event = handler.getClass().getAnnotation(Event.class);
+            if (event.banned()) {
+                this.banned.add(event.packet());
+            }
+            this.requests.put(event.packet(), handler);
+        }
     }
 }
--- src/com/aqworlds/world/World.java
+++ src/com/aqworlds/world/World.java
@@ -4,19 +4,24 @@ import com.aqworlds.AQWorlds;
 import com.aqworlds.config.ConfigData;
 import com.aqworlds.network.RequestManage;
 import it.gotoandplay.smartfoxserver.data.Zone;
-import it.gotoandplay.smartfoxserver.extensions.AbstractExtension;
+import net.sf.json.JSONObject;
+
+import java.nio.channels.SocketChannel;
+import java.util.LinkedList;
 
 public class World {
 
     private AQWorlds aqworlds;
     public ConfigData config;
     public RequestManage requestManage;
-    private final AbstractExtension extension;
     public final Zone zone;
 
     public World(AQWorlds aqworlds) {
         this.aqworlds = aqworlds;
-        this.extension = aqworlds;
         this.zone = this.aqworlds.helper.getZone(this.aqworlds.getOwnerZone());
+    }
+
+    public void send(JSONObject params, LinkedList<SocketChannel> channels) {
+        this.aqworlds.sendResponse(params, -1, null, channels);
     }
 }
